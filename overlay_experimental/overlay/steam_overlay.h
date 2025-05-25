6f61fe1109d4b583f8c8f959a523949fdeca1e41
aylz10 <aylz10@126.com>
2025-05-25 10:47:16 +0800

    Old version ingame_overlay  & 6fc33cec

--- overlay_experimental/overlay/steam_overlay.h
+++ overlay_experimental/overlay/steam_overlay.h
@@ -72,8 +72,8 @@ struct Overlay_Achievement
     bool hidden{};
     bool achieved{};
     uint32 unlock_time{};
-    InGameOverlay::RendererResource_t* icon{};
-    InGameOverlay::RendererResource_t* icon_gray{};
+    std::weak_ptr<uint64_t> icon{};
+    std::weak_ptr<uint64_t> icon_gray{};
     int icon_handle = Settings::UNLOADED_IMAGE_HANDLE;
     int icon_gray_handle = Settings::UNLOADED_IMAGE_HANDLE;
 };
